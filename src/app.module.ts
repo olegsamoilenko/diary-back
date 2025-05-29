b987f84a2d68a57240576b5c0237504363286055
olegsamoilenko <soniac@ukr.net>
2025-05-29 18:50:01 +0300

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: process.env.NODE_ENV !== 'development',
+      extra:
+        process.env.NODE_ENV === 'development'
+          ? {}
+          : {
+              ssl: {
+                rejectUnauthorized: false,
+              },
+            },
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
