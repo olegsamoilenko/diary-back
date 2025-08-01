c6c93c0e62c64c04b929be36d2a18312a8f5fabe
olegsamoilenko <soniac@ukr.net>
2025-08-01 16:34:55 +0300

    subscription

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,48 +1,36 @@
-import * as nodemailer from 'nodemailer';
 import { Injectable } from '@nestjs/common';
 import * as handlebars from 'handlebars';
 import * as fs from 'fs';
 import * as path from 'path';
-import { SendMailOptions } from 'nodemailer';
+import Mailgun from 'mailgun.js';
+import FormData from 'form-data';
+
+type MailgunClient = ReturnType<Mailgun['client']>;
 
 @Injectable()
 export class EmailsService {
-  private transporter: nodemailer.Transporter;
-
+  private mg: MailgunClient;
   constructor() {
-    console.log('SMTP HOST:', process.env.SMTP_HOST);
-    this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-      // tls: {
-      //   ciphers: 'SSLv3'
-      // }
-    }) as nodemailer.Transporter;
+    this.mg = new Mailgun(FormData).client({
+      username: 'api',
+      key: process.env.MAILGUN_API_KEY!,
+    });
   }
 
-  async send(
-    to: string[],
-    subject: string,
-    template: string,
-    context?: any,
-    headers?: SendMailOptions['headers'],
-  ) {
+  async send(to: string[], subject: string, template: string, context?: any) {
+    const domain = process.env.MAILGUN_DOMAIN!;
+    const from = process.env.MAILGUN_FROM_EMAIL!;
     const html = this.loadTemplate(`${template}.hbs`)(context);
 
     try {
-      await this.transporter.sendMail({
+      const result = await this.mg.messages.create(domain, {
         to,
         subject,
-        from: process.env.SMTP_FROM,
+        from: `Nemory <${from}>`,
         html,
-        headers,
       });
       console.log('Email sent successfully');
+      return result;
     } catch (error: unknown) {
       let message = 'Unknown error';
       if (error instanceof Error) {
