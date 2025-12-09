a131941c64e9c8654b57788fae8d422734c1b132
olegsamoilenko <soniac@ukr.net>
2025-12-09 16:07:49 +0200

    save

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,24 @@ import type { Express } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'https://nemoryai.com',
+    'https://www.nemoryai.com',
+    'http://localhost:3000',
+  ];
+
   app.enableCors({
-    origin: '*',
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'), false);
+    },
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders:
