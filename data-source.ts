74ae82e5e7c678bc3e0aa355ea656e4c99d0011d
olegsamoilenko <soniac@ukr.net>
2026-05-16 11:55:53 +0300

    commit

--- data-source.ts
+++ data-source.ts
@@ -1,13 +1,17 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
+const useSsl = process.env.DB_SSL === 'true';
+
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 5432),
+  port: Number(process.env.DB_PORT || 25060),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+
+  ssl: useSsl ? { rejectUnauthorized: false } : false,
 
   entities: ['dist/**/*.entity.js', 'dist/**/entities/*.js'],
   migrations: ['dist/migrations/*.js'],
